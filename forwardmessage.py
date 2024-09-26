e7ae85c8ab981e366f1a8fef9123026be0eff9f4
pinrepos <it.ukr@peopleinneed.net>
2024-09-26 16:39:11 +0300

    Update forwardmessage.py

--- forwardmessage.py
+++ forwardmessage.py
@@ -6,14 +6,14 @@ api_hash = ''
 client = TelegramClient('anon', api_id, api_hash)
 client.start()
 
-
+# channel id of the channels from which messages are sent, forwarding bot
 @client.on(events.NewMessage((-1001223955273, -1001641260594)))
 async def main(event):
     await client.forward_messages(7402205329, event.message)
 
 # client.run_until_disconnected()
 
-
+#bot forward to final channel
 @client.on(events.NewMessage(7402205329))
 async def main(event):
     await client.forward_messages(-1002425747864, event.message)
